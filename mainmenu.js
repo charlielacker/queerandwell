1641db14c403ffbc44f162d8ecf358245139be67
charlielacker <98198358+charlielacker@users.noreply.github.com>
2023-12-29 12:06:13 -0500

    mainmenu.js

---
+++ mainmenu.js
@@ -0,0 +1,8 @@
+/* place the navigation on the page */
+function openNav() {
+    document.getElementById("mainmenu").style.right = "0";
+}
+/* place the navigation off the page */
+function closeNav() {
+    document.getElementById("mainmenu").style.right = "-10rem";
+}
